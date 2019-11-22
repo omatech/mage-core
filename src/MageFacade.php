11a4538183adb4abb51cc0f8f54783b3ce11f68c
Christian <cbohollo@omatech.com>
2019-11-22 16:39:28 +0000

    Apply fixes from StyleCI

--- src/MageFacade.php
+++ src/MageFacade.php
@@ -4,16 +4,16 @@ namespace Omatech\Mage\Core;
 
 use Exception;
 use Illuminate\Support\Facades\Facade;
-use Omatech\Mage\Core\Facades\RoleFacade;
-use Omatech\Mage\Core\Facades\UserFacade;
 use Omatech\Mage\Core\Facades\PermissionFacade;
+use Omatech\Mage\Core\Facades\RoleFacade;
 use Omatech\Mage\Core\Facades\TranslationFacade;
+use Omatech\Mage\Core\Facades\UserFacade;
 
 class MageFacade extends Facade
 {
     public static function domain(string $domain)
     {
-        switch($domain) {
+        switch ($domain) {
             case 'Permission':
                 $facade = PermissionFacade::class;
                 break;
